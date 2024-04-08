5b82153d3e2576943cce4b539370d43719811b5e
pcazin <pablocazin.dev@gmail.com>
2024-04-08 20:26:16 +0200

    Ajout de la fonctionnalité de traduction

--- python/tennis1.py
+++ python/tennis1.py
@@ -1,12 +1,43 @@
-# -*- coding: utf-8 -*-
-
 class TennisGame1:
 
-    def __init__(self, p1_name, p2_name):
+    def __init__(self, p1_name, p2_name, language):
         self.p1_name = p1_name
         self.p2_name = p2_name
         self.p1_points = 0
         self.p2_points = 0
+        self.language = language
+
+        # Configuration des textes en français et en anglais
+        self.config = {
+            "fr": {
+                "Love": "Zéro",
+                "Fifteen": "Quinze",
+                "Thirty": "Trente",
+                "Forty": "Quarante",
+                "Love-All": "Égalité",
+                "Fifteen-All": "Égalité",
+                "Thirty-All": "Égalité",
+                "Deuce": "Égalité",
+                "Advantage": "Avantage",
+                "Win": "Victoire pour"
+            },
+            "en": {
+                "Love": "Love",
+                "Fifteen": "Fifteen",
+                "Thirty": "Thirty",
+                "Forty": "Forty",
+                "Love-All": "Love-All",
+                "Fifteen-All": "Fifteen-All",
+                "Thirty-All": "Thirty-All",
+                "Deuce": "Deuce",
+                "Advantage": "Advantage",
+                "Win": "Win for"
+            }
+        }
+
+        # Vérification de la langue
+        if self.language not in self.config:
+            raise ValueError("Langue non prise en charge")
 
     # Ajoute un point au joueur correspondant
     def won_point(self, player_name):
@@ -15,37 +46,44 @@ class TennisGame1:
         else:
             self.p2_points += 1
 
+    # Obtenir la traduction d'un text
+    def get_translate(self, text):
+        return self.config[self.language].get(text, text)
+
     # Retourne le score en fonction du score des joueurs
     def score(self):
         result = ""
         temp_score=0
-        if (self.p1_points==self.p2_points):
-            result = {
+        if self.p1_points == self.p2_points:
+            result = self.get_translate({
                 0 : "Love-All",
                 1 : "Fifteen-All",
                 2 : "Thirty-All",
-            }.get(self.p1_points, "Deuce")
-        elif (self.p1_points>=4 or self.p2_points>=4):
+            }.get(self.p1_points, "Deuce"))
+
+        elif (self.p1_points >= 4 or self.p2_points >= 4):
             minus_result = self.p1_points - self.p2_points
             if (minus_result == 1):
-                result = f"Advantage {self.p1_name}"
+                result = f"{self.get_translate('Advantage')} {self.get_translate(self.p1_name)}"
             elif (minus_result == -1):
-                result = f"Advantage {self.p2_name}"
+                result = f"{self.get_translate('Advantage')} {self.get_translate(self.p2_name)}"
             elif (minus_result >= 2):
-                result = f"Win for {self.p1_name}"
+                result = f"{self.get_translate('Win')} {self.get_translate(self.p1_name)}"
             else:
-                result = f"Win for {self.p2_name}"
+                result = f"{self.get_translate('Win')} {self.get_translate(self.p2_name)}"
         else:
             for i in range(1,3):
-                if (i==1):
+                if (i == 1):
                     temp_score = self.p1_points
                 else:
-                    result+="-"
+                    result += "-"
                     temp_score = self.p2_points
-                result += {
+
+                result += self.get_translate({
                     0 : "Love",
                     1 : "Fifteen",
                     2 : "Thirty",
                     3 : "Forty",
-                }[temp_score]
+                }[temp_score])
+
         return result
