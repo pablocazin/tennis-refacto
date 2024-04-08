7eed5a5d6ed722474e42c7b0da5831bcb4341534
pcazin <pablocazin.dev@gmail.com>
2024-04-08 20:14:50 +0200

    Refactoring des conventions de nommage.

--- python/tennis1.py
+++ python/tennis1.py
@@ -2,48 +2,48 @@
 
 class TennisGame1:
 
-    def __init__(self, player1Name, player2Name):
-        self.player1Name = player1Name
-        self.player2Name = player2Name
-        self.p1points = 0
-        self.p2points = 0
+    def __init__(self, p1_name, p2_name):
+        self.p1_name = p1_name
+        self.p2_name = p2_name
+        self.p1_points = 0
+        self.p2_points = 0
 
-    def won_point(self, playerName):
-        if playerName == "player1":
-            self.p1points += 1
+    def won_point(self, player_name):
+        if player_name == "player1":
+            self.p1_points += 1
         else:
-            self.p2points += 1
+            self.p2_points += 1
 
     def score(self):
         result = ""
-        tempScore=0
-        if (self.p1points==self.p2points):
+        temp_score=0
+        if (self.p1_points==self.p2_points):
             result = {
                 0 : "Love-All",
                 1 : "Fifteen-All",
                 2 : "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif (self.p1points>=4 or self.p2points>=4):
-            minusResult = self.p1points-self.p2points
-            if (minusResult==1):
+            }.get(self.p1_points, "Deuce")
+        elif (self.p1_points>=4 or self.p2_points>=4):
+            minus_result = self.p1_points-self.p2_points
+            if (minus_result==1):
                 result ="Advantage player1"
-            elif (minusResult ==-1):
+            elif (minus_result ==-1):
                 result ="Advantage player2"
-            elif (minusResult>=2):
+            elif (minus_result>=2):
                 result = "Win for player1"
             else:
                 result ="Win for player2"
         else:
             for i in range(1,3):
                 if (i==1):
-                    tempScore = self.p1points
+                    temp_score = self.p1_points
                 else:
                     result+="-"
-                    tempScore = self.p2points
+                    temp_score = self.p2_points
                 result += {
                     0 : "Love",
                     1 : "Fifteen",
                     2 : "Thirty",
                     3 : "Forty",
-                }[tempScore]
+                }[temp_score]
         return result
