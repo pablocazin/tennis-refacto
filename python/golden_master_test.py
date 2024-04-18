3a26008ddfb3bed2a3e153638bc2527440691a4b
paul-brd02 <paul.bardou@live.fr>
2024-04-18 12:39:54 +0200

    Prise en compte de la fonctionnalité de la langue dans le golden master

--- python/golden_master_test.py
+++ python/golden_master_test.py
@@ -1,15 +1,16 @@
 import unittest
 
-from tennis4 import TennisGame4
+from tennis1 import TennisGame1
 
 
 class GoldenMasterTest(unittest.TestCase):
 
-    DIR = "C:\\Users\\ateli\\Sources\\Formations\\EPSI Toulouse\\2023-2024\\I1 DEV\\TennisKata$\\python\\golden-master";
-
+    # Modifier en foncion de son chemin
+    DIR = "C:\\EPSI\\Exo\\tennis-refacto\\python\\golden-master"
+    languages = ['fr', 'en']
     @staticmethod
-    def play_game(p1Points, p2Points, p1Name, p2Name):
-        game = TennisGame4(p1Name, p2Name)
+    def play_game(p1Points, p2Points, p1Name, p2Name, language):
+        game = TennisGame1(p1Name, p2Name, language)
         for i in range(max(p1Points, p2Points)):
             if i < p1Points:
                 game.won_point(p1Name)
@@ -17,25 +18,27 @@ class GoldenMasterTest(unittest.TestCase):
                 game.won_point(p2Name)
         return game.score()
 
-    def make_file_name(self, score_player_1, score_player_2):
-        return f"{self.DIR}\\{score_player_1}_{score_player_2}.txt"
+    def make_file_name(self, language, score_player_1, score_player_2):
+        return f"{self.DIR}\\{language}_{score_player_1}_{score_player_2}.txt"
 
-    def _test_record(self):
-        for score_player_1 in list(range(0, 16)):
-            for score_player_2 in list(range(0, 16)):
-                with self.subTest(f"{score_player_1}, {score_player_2}"):
-                    sortie = self.play_game(score_player_1, score_player_2, "player1", "player2")
-                    file = open(self.make_file_name(score_player_1, score_player_2), "w")
-                    file.writelines(sortie)
-                    file.close()
+    def test_record(self):
+        for language in self.languages:
+            for score_player_1 in list(range(0, 16)):
+                for score_player_2 in list(range(0, 16)):
+                    with self.subTest(f"{score_player_1}, {score_player_2}"):
+                        sortie = self.play_game(score_player_1, score_player_2, "player1", "player2", language)
+                        file = open(self.make_file_name(language, score_player_1, score_player_2), "w")
+                        file.writelines(sortie)
+                        file.close()
 
     def test_replay(self):
-        for score_player_1 in list(range(0, 16)):
-            for score_player_2 in list(range(0, 16)):
-                with self.subTest(f"{score_player_1}, {score_player_2}"):
-                    sortie = self.play_game(score_player_1, score_player_2, "player1", "player2")
-                    file = open(self.make_file_name(score_player_1, score_player_2), "r")
-                    attendu = file.read()
-                    file.close()
-                    self.assertEqual(attendu, sortie)
+        for language in self.languages:
+            for score_player_1 in list(range(0, 16)):
+                for score_player_2 in list(range(0, 16)):
+                    with self.subTest(f"{score_player_1}, {score_player_2}"):
+                        sortie = self.play_game(score_player_1, score_player_2, "player1", "player2", language)
+                        file = open(self.make_file_name(language, score_player_1, score_player_2), "r")
+                        attendu = file.read()
+                        file.close()
+                        self.assertEqual(attendu, sortie)
 
