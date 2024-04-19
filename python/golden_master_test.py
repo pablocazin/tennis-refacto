394391c686702a8d93e3bb249cbe1715892fa4d4
paul-brd02 <paul.bardou@live.fr>
2024-04-19 12:25:27 +0200

    Ajout d'une classe pour gérer les langues

--- python/golden_master_test.py
+++ python/golden_master_test.py
@@ -1,19 +1,19 @@
 import unittest
 
-from tennis1 import TennisGame1, Player
+from tennis1 import TennisGame1, Player, Language
 
 
 class GoldenMasterTest(unittest.TestCase):
 
     # Modifier en foncion de son chemin de dossier
     DIR = "C:\\EPSI\\Exo\\tennis-refacto\\python\\golden-master"
-    languages = ['fr', 'en']
+    languages = [Language('French'), Language('English')]
 
     player_1 = Player("player1")
     player_2 = Player("player2")
 
     @staticmethod
-    def play_game(player1: Player, player2: Player, language):
+    def play_game(player1: Player, player2: Player, language: Language):
         game = TennisGame1(player1, player2, language)
         for i in range(max(player1.point, player2.point)):
             if i < player1.point:
@@ -33,7 +33,7 @@ class GoldenMasterTest(unittest.TestCase):
                     self.player_2.setScore(score_player_2)
                     with self.subTest(f"{self.player_1.point}, {self.player_2.point}"):
                         sortie = self.play_game(self.player_1, self.player_2, language)
-                        file = open(self.make_file_name(language, self.player_1.point, self.player_2.point), "w")
+                        file = open(self.make_file_name(language.name, self.player_1.point, self.player_2.point), "w")
                         file.writelines(sortie)
                         file.close()
 
@@ -45,7 +45,7 @@ class GoldenMasterTest(unittest.TestCase):
                     self.player_2.setScore(score_player_2)
                     with self.subTest(f"{self.player_1.point}, {self.player_2.point}"):
                         sortie = self.play_game(self.player_1, self.player_2, language)
-                        file = open(self.make_file_name(language, self.player_1.point, self.player_2.point), "r")
+                        file = open(self.make_file_name(language.name, self.player_1.point, self.player_2.point), "r")
                         attendu = file.read()
                         file.close()
                         self.assertEqual(attendu, sortie)
--- python/tennis1.py
+++ python/tennis1.py
@@ -1,3 +1,5 @@
+import json
+import os
 # ========================================================================================
 # Toutes les modifications sont expliquées dans le fichier README.md à la racine du projet
 # ========================================================================================
@@ -10,46 +12,53 @@ class Player:
     def setScore(self, value: int):
         self.point = value
 
+class Language:
+    def __init__(self, language: str):
+        self.name = language
+        self.love = None
+        self.fifteen = None
+        self.thirty = None
+        self.forty = None
+        self.love_all = None
+        self.fifteen_all = None
+        self.thirty_all = None
+        self.deuce = None
+        self.advantage = None
+        self.win = None
+        self.load_languages('languages.json')
+
+    def load_languages(self, config_file):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir, config_file)
+
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            for lang_data in data:
+                if lang_data['name'] == self.name:
+                    translate = lang_data
+                
+            if not translate:
+                raise ValueError("Language not supported")
+
+            self.love = translate['Love']
+            self.fifteen = translate['Fifteen']
+            self.thirty = translate['Thirty']
+            self.forty = translate['Forty']
+            self.love_all = translate['Love-All']
+            self.fifteen_all = translate['Fifteen-All']
+            self.thirty_all = translate['Thirty-All']
+            self.deuce = translate['Deuce']
+            self.advantage = translate['Advantage']
+            self.win = translate['Win']
+
 class TennisGame1:
 
-    def __init__(self, player1: Player, player2: Player, language):
+    def __init__(self, player1: Player, player2: Player, language: Language):
         self.p1_name = player1.name
         self.p2_name = player2.name
         self.p1_points = player1.point
         self.p2_points = player2.point
         self.language = language
-
-        # Configuration des textes en français et en anglais
-        self.config = {
-            "fr": {
-                "Love": "Zéro",
-                "Fifteen": "Quinze",
-                "Thirty": "Trente",
-                "Forty": "Quarante",
-                "Love-All": "Égalité",
-                "Fifteen-All": "Égalité",
-                "Thirty-All": "Égalité",
-                "Deuce": "Égalité",
-                "Advantage": "Avantage",
-                "Win": "Victoire pour"
-            },
-            "en": {
-                "Love": "Love",
-                "Fifteen": "Fifteen",
-                "Thirty": "Thirty",
-                "Forty": "Forty",
-                "Love-All": "Love-All",
-                "Fifteen-All": "Fifteen-All",
-                "Thirty-All": "Thirty-All",
-                "Deuce": "Deuce",
-                "Advantage": "Advantage",
-                "Win": "Win for"
-            }
-        }
-
-        # Vérification de la langue
-        if self.language not in self.config:
-            raise ValueError("Langue non prise en charge")
 
     # Ajoute un point au joueur correspondant
     def won_point(self, player_name):
@@ -58,31 +67,27 @@ class TennisGame1:
         else:
             self.p2_points += 1
 
-    # Obtenir la traduction d'un text
-    def get_translate(self, text):
-        return self.config[self.language].get(text, text)
-
     # Retourne le score en fonction du score des joueurs
     def score(self):
         result = ""
         temp_score=0
         if self.p1_points == self.p2_points:
-            result = self.get_translate({
-                0 : "Love-All",
-                1 : "Fifteen-All",
-                2 : "Thirty-All",
-            }.get(self.p1_points, "Deuce"))
+            result = {
+                0 : self.language.love_all,
+                1 : self.language.fifteen_all,
+                2 : self.language.thirty_all,
+            }.get(self.p1_points, self.language.deuce)
 
         elif (self.p1_points >= 4 or self.p2_points >= 4):
             minus_result = self.p1_points - self.p2_points
             if (minus_result == 1):
-                result = f"{self.get_translate('Advantage')} {self.get_translate(self.p1_name)}"
+                result = f"{self.language.advantage} {self.p1_name}"
             elif (minus_result == -1):
-                result = f"{self.get_translate('Advantage')} {self.get_translate(self.p2_name)}"
+                result = f"{self.language.advantage} {self.p2_name}"
             elif (minus_result >= 2):
-                result = f"{self.get_translate('Win')} {self.get_translate(self.p1_name)}"
+                result = f"{self.language.win} {self.p1_name}"
             else:
-                result = f"{self.get_translate('Win')} {self.get_translate(self.p2_name)}"
+                result = f"{self.language.win} {self.p2_name}"
         else:
             for i in range(1,3):
                 if (i == 1):
@@ -91,11 +96,11 @@ class TennisGame1:
                     result += "-"
                     temp_score = self.p2_points
 
-                result += self.get_translate({
-                    0 : "Love",
-                    1 : "Fifteen",
-                    2 : "Thirty",
-                    3 : "Forty",
-                }[temp_score])
+                result += {
+                    0 : self.language.love,
+                    1 : self.language.fifteen,
+                    2 : self.language.thirty,
+                    3 : self.language.forty,
+                }[temp_score]
 
         return result
